53c9e85d8d9293c4506e9852432349ce131f4956
SampoAho <fsaah003@edu.xamk.fi>
2026-02-17 12:34:20 +0200

    Add validation and tests with CI/CD pipeline

--- SimpleExample.Infrastructure/Repositories/GenericRepository.cs
+++ SimpleExample.Infrastructure/Repositories/GenericRepository.cs
@@ -28,6 +28,7 @@ public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
 
     public async Task<T> AddAsync(T entity)
     {
+        // EI aseteta Id:tä, FirstName, LastName, Email - entity on jo validi!
         entity.Id = Guid.NewGuid();
         entity.CreatedAt = DateTime.UtcNow;
         entity.UpdatedAt = DateTime.UtcNow;
